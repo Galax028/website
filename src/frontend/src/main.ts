4cac77eda2edabd26873f3aaa3bf8fdddf1f89b4
Phawat Suksiriwan <phawat@suksiriwan.com>
2024-08-10 22:48:52 +0700

    feat(frontend): added not found page and configure build correctly

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -10,18 +10,25 @@ function getCurrentTheme(): Theme {
 function main(): void {
   console.log("Hello, world!");
 
-  // const currentTheme = getCurrentTheme();
-  // if (currentTheme === "dark") {
-  //   localStorage.setItem("theme", "dark");
-  //   document.documentElement.classList.add("dark");
-  // } else {
-  //   localStorage.setItem("theme", "light");
-  //   document.documentElement.classList.remove("dark");
-  // }
-
   const themeToggleBtn = document.getElementById(
     "theme-toggle-btn",
   ) as HTMLButtonElement;
+  const lightModeIcon = document.querySelector(
+    "#theme-toggle-btn > svg.light-mode-icon",
+  ) as SVGElement;
+  const darkModeIcon = document.querySelector(
+    "#theme-toggle-btn > svg.dark-mode-icon",
+  ) as SVGElement;
+
+  const initialTheme = getCurrentTheme();
+  if (initialTheme === "dark") {
+    lightModeIcon.classList.add("hidden");
+    darkModeIcon.classList.remove("hidden");
+  } else {
+    darkModeIcon.classList.add("hidden");
+    lightModeIcon.classList.remove("hidden");
+  }
+
   themeToggleBtn.addEventListener("click", (): void => {
     const currentTheme = getCurrentTheme();
 
@@ -29,10 +36,14 @@ function main(): void {
       case "dark":
         localStorage.setItem("theme", "light");
         document.documentElement.classList.remove("dark");
+        darkModeIcon.classList.add("hidden");
+        lightModeIcon.classList.remove("hidden");
         break;
       case "light":
         localStorage.setItem("theme", "dark");
         document.documentElement.classList.add("dark");
+        lightModeIcon.classList.add("hidden");
+        darkModeIcon.classList.remove("hidden");
         break;
     }
   });
